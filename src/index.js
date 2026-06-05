6f3ab83117775cbce13fe3af4848f8d04e1142c3
fourpunchman1 <aa0910024389@gmail.com>
2026-06-05 13:50:27 +0800

    fix(router): set BrowserRouter basename for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './scss/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL || '/zlcsc23'}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
